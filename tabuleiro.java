ca43cd6e77f91ab8e29f47a8b333fde1a7901025
jamyllesouza634-hub <jamyllesouza634@gmail.com>
2025-08-07 15:09:28 -0300

    possibilidades de jogadas de acordo com a resposta do jogador e mensagem de erro em caso de jogada impossível

--- tabuleiro.java
+++ tabuleiro.java
@@ -26,7 +26,152 @@ public class tabuleiro  {
                 System.out.println("jogada inválida, tente novamente");
             }
         
-            
+            if ((resposta1 == "x1" && p1.P == "x1") || resposta1 == "x2" && p1.P == "x2"){
+
+        p1.disponibilidade = true;
+        p1.P = "vazio";
+
+        
+        if (p2.disponibilidade == true){
+
+            p2.disponibilidade = false;
+            p2.P = resposta1;
+        }
+
+            if (p3.disponibilidade == true){
+    
+                p3.disponibilidade = false;
+                p3.P = resposta1;
+            }
+                
+                else{
+                    
+                    System.out.println("está peça está presa, escolha outra");
+                }
+    }
+
+   
+    if ((resposta1 == "x1" && p2.P == "x1") || resposta1 == "x2" && p2.P == "x2") {
+        
+        p2.disponibilidade = true;
+        p2.P = "vazio";
+
+        
+        if (p1.disponibilidade == true) {
+
+            p1.disponibilidade = false;
+            p1.P = resposta1;
+        }
+            if (p3.disponibilidade == true) {
+        
+                p3.disponibilidade = false;
+                p3.P = resposta1;
+            }
+                if (p5.disponibilidade == true) {
+
+                    p5.disponibilidade = false;
+                    p5.P = resposta1;
+                }
+                   
+                    else{
+                    
+                    System.out.println("está peça está presa, escolha outra");
+                }
+    }
+
+    
+    if ((resposta1 == "x1" && p3.P == "x1") || resposta1 == "x2" && p3.P == "x2") {
+
+        p3.disponibilidade = true;
+        p3.P = "vazio";
+
+        
+        if (p1.disponibilidade == true) {
+
+            p1.disponibilidade = false;
+            p1.P = resposta1;
+        }
+            if (p2.disponibilidade == true){
+
+                p2.disponibilidade = false;
+                p2.P = resposta1;
+            }
+                if (p3.disponibilidade == true){
+    
+                    p3.disponibilidade = false;
+                    p3.P = resposta1;
+                }
+                    if (p4.disponibilidade = true) {
+
+                        p4.disponibilidade = false;
+                        p4.P = resposta1;
+                    }
+                        if (p5.disponibilidade == true) {
+
+                            p5.disponibilidade = false;
+                            p5.P = resposta1;
+                        }
+                            
+                            else{
+                    
+                                System.out.println("está peça está presa, escolha outra");
+                            }
+                        
+    }
+
+    
+    if((resposta1 == "x1" && p4.P == "x1") || resposta1 == "x2" && p4.P == "x2" ){
+
+        p4.disponibilidade = true;
+        p4.P = "vazio";
+
+       
+        if(p3.disponibilidade = true){
+
+            p3.disponibilidade = false;
+            p3.P = resposta1;
+        }
+            if(p5.disponibilidade = true){
+
+                p5.disponibilidade = false;
+                p5.P = resposta1;
+            }
+               
+                else{
+                    
+                    System.out.println("está peça está presa, escolha outra");
+                } 
+    }
+
+  
+   if ((resposta1 == "x1" && p5.P == "x1") || resposta1 == "x2" && p5.P == "x2") {
+
+        p5.disponibilidade = true;
+        p5.P = "vazio";
+
+        
+        if (p2.disponibilidade == true){
+
+                p2.disponibilidade = false;
+                p2.P = resposta1;
+        }
+                if(p3.disponibilidade = true){
+
+                    p3.disponibilidade = false;
+                    p3.P = resposta1;
+                }
+                    if (p4.disponibilidade = true) {
+
+                        p4.disponibilidade = false;
+                        p4.P = resposta1;
+                    }
+      
+                        else{
+                    
+                            System.out.println("está peça está presa, escolha outra");
+                        }
+   }
+
 
             input.close();
         }
