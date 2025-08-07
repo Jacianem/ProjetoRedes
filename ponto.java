4403641e4cff67c0856d0e6fe33a4a2b67b6e70a
Jacianem <melo.jaciane@escolar.irfn.edu.br>
2025-08-07 14:28:06 -0300

    criação da classe ponto do tabuleiro

--- ponto.java
+++ ponto.java
@@ -1,14 +1,12 @@
-
+    // criação dos pontos no tabuleiro
 public class ponto{
 
+String P;
+boolean disponibilidade;
+ponto(String P, boolean disponibilidade){
+    
+this.P = P;   
+this.disponibilidade = disponibilidade;
+};
 
-
-
-
-
-
-
-
-
-
-}
+}  
