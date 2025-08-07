d3ec0d425166dc12de5e4588419b854eab3d494c
Jacianem <melo.jaciane@escolar.irfn.edu.br>
2025-08-07 14:37:01 -0300

    criação dos objetos pontos no tabuleiro

--- tabuleiro.java
+++ tabuleiro.java
@@ -6,6 +6,16 @@ public class tabuleiro  {
 
 
 
+       Scanner input = new Scanner(System.in);
+        
+        ponto p1 = new ponto("x1", false);
+        ponto p2 = new ponto("y1", false);
+        ponto p3 = new ponto("vazio", true);
+        ponto p4 = new ponto("x2", false);
+        ponto p5 = new ponto("y2", false);
+
+
+
 
 
 
