0e56c127d2fcca77a77df5ba7c323c8985564dc8
Jacianem <melo.jaciane@escolar.irfn.edu.br>
2025-08-07 14:41:06 -0300

    solicitação de jogada ao jogador e mensagem de erro em caso de resposta inválida

--- tabuleiro.java
+++ tabuleiro.java
@@ -15,9 +15,20 @@ public class tabuleiro  {
         ponto p5 = new ponto("y2", false);
 
 
+        while(true){
 
+        
+            System.out.println("sua vez, escolha qual peça quer mover, x1 ou x2?");
+            String resposta1 = input.nextLine();
 
+                
+            if (resposta1 != "x2" || resposta1 != "x1") {
+                System.out.println("jogada inválida, tente novamente");
+            }
+        
+            
 
-
+            input.close();
+        }
     }
 }
